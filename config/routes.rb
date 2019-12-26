bb88f1c0e9f613c0b6a62ac6f8f3729f37867e2a
cato b <cato.benjaminsen@gmail.com>
2019-12-26 10:28:58 +0100

    Add shout and user views

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,17 @@ Rails.application.routes.draw do
   end
 
   root to: 'homes#show'
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resources :shouts, only: [:create]
+  resources :passwords, controller: 'clearance/passwords', only: %i[create new]
+  resources :shouts, only: %i[create show]
   resource :session, only: [:create]
 
-  resources :users, only: [:create] do
+  resources :users, only: %i[create show] do
     resource :password,
-      controller: "clearance/passwords",
-      only: [:edit, :update]
+      controller: 'clearance/passwords',
+      only: %i[edit update]
   end
 
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
-  get "/sign_up" => "users#new", as: "sign_up"
+  get '/sign_in' => 'sessions#new', as: 'sign_in'
+  delete '/sign_out' => 'sessions#destroy', as: 'sign_out'
+  get '/sign_up' => 'users#new', as: 'sign_up'
 end
